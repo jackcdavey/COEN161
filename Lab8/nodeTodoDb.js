de6975f960c1cf6ec4305cb598d26a09a27367ba
Jack Davey <jdavey@scu.edu>
2021-03-08 20:22:52 -0800

    nodeTodoDb file now functional

--- Lab8/nodeTodoDb.js
+++ Lab8/nodeTodoDb.js
@@ -3,11 +3,29 @@ const mysql = require('mysql');
 const config = require('./config').config;
 
 exports.addTodo = function (sessionId, todo, callback) {
-  const con = mysql.createConnection(config);
-  // call con.connect();
+	const con = mysql.createConnection(config);
+	con.connect(function(err){
+		if (err)
+			return callback(err);
+		console.log("Connection Established");
+		var sql = "INSERT INTO Todos (description, sessionId) VALUES (?,?);";
+		con.query(sql, [todo,sessionId], function(err, result){
+			return callback(err);
+		});
+		con.end();
+	});
 };
 
 exports.getTodos = function (sessionId, callback) {
-  const con = mysql.createConnection(config);
-  // call con.connect();  
+	const con = mysql.createConnection(config);
+	con.connect(function(err){
+		if (err) 
+			return callback(err);
+		console.log("Connection Established");
+		var sql = "SELECT * FROM Todos WHERE sessionId = ?;"
+		con.query(sql,[sessionId], function(err, result) {
+			return callback(err, results);
+		});
+		con.end();
+	});
 };
